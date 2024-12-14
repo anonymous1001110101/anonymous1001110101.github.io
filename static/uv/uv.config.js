53fffeb00e0c4acc0b552c647fb68d563d5fdda2
anonymous1001110101 <petezahgames@gmail.com>
2024-12-14 12:55:23 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 
 self.__uv$config = {
     prefix: '/static/ixl/',
-    bare:'https://static.99.135.156.178.clients.your-server.de/bare/',
+    bare:'http://bare.petezahgames.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
